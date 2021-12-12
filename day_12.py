c609ade1278d20faf76cfeeb3007bba8f4bdb222
Yannick Engel <ukfbi@student.kit.edu>
2021-12-12 15:49:27 +0100

    initia setup of all files

---
+++ day_12.py
@@ -0,0 +1,14 @@
+from input_reader import read
+
+input_lines = read.input(12)
+print(input_lines)
+
+def parseInput(input):
+    ...
+
+DATA = parseInput(input_lines)
+
+#============================= Part One =============================
+
+
+#============================= Part Two =============================
---
+++ day_16.py
@@ -0,0 +1,14 @@
+from input_reader import read
+
+input_lines = read.input(16)
+print(input_lines)
+
+def parseInput(input):
+    ...
+
+DATA = parseInput(input_lines)
+
+#============================= Part One =============================
+
+
+#============================= Part Two =============================
