e6e11eb38c9bf95e274b17cf7a284e012f975da1
Yannick Engel <ukfbi@student.kit.edu>
2021-12-12 15:46:41 +0100

    Day 01

---
+++ day_01.py
@@ -0,0 +1,26 @@
+from input_reader import read
+import numpy as np
+import re
+
+input_lines = read.input(1)
+
+def parseInput(input):
+    return np.array(list(map(int, re.findall(r"(\d+)", input))))
+
+DATA = parseInput(input_lines)
+
+#============================= Part One =============================
+count = 0
+for i in range(1,DATA.size):
+    if DATA[i] > DATA[i-1]:
+        count +=1
+print(count)
+
+#============================= Part Two =============================
+def get3sum(index):
+    return DATA[index] + DATA[index+ 1] + DATA[index+ 2]
+count = 0
+for i in range(1,DATA.size - 2):
+    if get3sum(i) > get3sum(i-1):
+        count +=1
+print(count)
