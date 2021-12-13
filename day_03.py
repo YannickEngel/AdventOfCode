abb16e42bee23d8e9855c2dec46d35613da0162c
Yannick Engel <ukfbi@student.kit.edu>
2021-12-13 16:34:27 +0100

    redone day 3 and 6

--- day_03.py
+++ day_03.py
@@ -1,10 +1,12 @@
 from input_reader import read
+import re
+import numpy as np
 
-input_lines = read.input(3)
+input_lines = read.inputLines(3)
 print(input_lines)
 
 def parseInput(input):
-    ...
+    return [re.findall(r"(\d)", line) for line in input]
 
 DATA = parseInput(input_lines)
 
--- day_06.py
+++ day_06.py
@@ -1,14 +1,22 @@
 from input_reader import read
 
 input_lines = read.input(6)
-print(input_lines)
-
 def parseInput(input):
-    ...
+    return[input.count(str(i)) for i in range(9)]
 
 DATA = parseInput(input_lines)
 
 #============================= Part One =============================
+def cycle(days, ages):
+    for _ in range(days):
+        new = ages[0]
+        for i in range(len(ages) - 1):
+            ages[i] = ages[i + 1]
+        ages[6] += new
+        ages[8] = new
+    return sum(ages)
 
-
+print(cycle(80, DATA.copy()))
 #============================= Part Two =============================
+
+print(cycle(256, DATA.copy()))
