b5106a5e0fc3a455a96423807853b0cd4fec055a
Yannick Engel <yannick@engel-family.de>
2021-12-14 09:54:05 +0100

    redone day 07

--- day_07.py
+++ day_07.py
@@ -1,14 +1,43 @@
+import statistics
+import math
 from input_reader import read
 
 input_lines = read.input(7)
-print(input_lines)
 
 def parseInput(input):
-    ...
+    return list(map(int, input.split(',')))
 
 DATA = parseInput(input_lines)
 
 #============================= Part One =============================
 
 
+
+def calFuel(arr, pos):
+    fuel = 0
+    for i in arr:
+        fuel += abs(i - pos)
+    return fuel
+
+min = min(DATA)
+max = max(DATA)
+
+position = statistics.median(DATA)
+print(f"Result Part 1: {calFuel(DATA,int(position))}")
+
+
 #============================= Part Two =============================
+def calFuelInc(arr, pos):
+    fuel = 0
+    for val in arr:
+        fuel += sum(list(range(1,abs(val-pos)+1)))
+    return fuel
+
+position = statistics.mean(DATA)
+fuel1 = calFuelInc(DATA, int(math.ceil(position)))
+fuel2 = calFuelInc(DATA, int(math.floor(position)))
+if (fuel1 < fuel2):
+    print(f"Result Part 2: {fuel1}")
+else:
+    print(f"Result Part 2: {fuel2}")
+
